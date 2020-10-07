99c218fe290efbe54d19bf4f1d587757e2aedd33
Therkelsen <37107686+Therkelsen@users.noreply.github.com>
2020-10-07 15:33:18 +0200

    Load image & print rgb
    Loaded custom image and print the RGB values for each pixel.
    
    Co-Authored-By: AugustBuchert <72136981+AugustBuchert@users.noreply.github.com>

--- Code/DrawingRobotMain.java
+++ Code/DrawingRobotMain.java
@@ -1,6 +1,18 @@
+
+import dk.sdu.mmmi.rd1.edgedetect.*;
+
+import Picture.java;
+import Luminance.java;
+import EdgeDetector.java;
+
 public class DrawingRobotMain {
     public static void main(String[] args) {
         //  This is where we will put our main code
+        Picture triangle = new Picture("D:/Files/GitHub/Drawing-Robot/Code/triangle_grayscale.jpg");
+        Picture danny = new Picture("D:/Files/GitHub/Drawing-Robot/Code/danny.jpg");
+        for (int col = 0; col < 1280; col++)
+            for (int row = 0; row < 720; row++)
+                System.out.println(danny.get(col,row));
         
     }
 }
