2c26ad21c698d27140c00b0fc30a3cefa7d34d51
Therkelsen <37107686+Therkelsen@users.noreply.github.com>
2020-10-07 18:39:32 +0200

    .

--- Code/DrawingRobotMain.java
+++ Code/DrawingRobotMain.java
@@ -1,4 +1,6 @@
 
+package dk.sdu.mmmi.rd1.edgedetect;
+
 import Picture.java;
 import Luminance.java;
 import EdgeDetector.java;
@@ -6,7 +8,6 @@ import EdgeDetector.java;
 public class DrawingRobotMain {
     public static void main(String[] args) {
         //  This is where we will put our main code
-        Picture triangle = new Picture("D:/Files/GitHub/Drawing-Robot/Assets/triangle_grayscale.jpg");
         Picture danny = new Picture("D:/Files/GitHub/Drawing-Robot/Assets/danny.jpg");
         for (int col = 0; col < 1280; col++)
             for (int row = 0; row < 720; row++)
