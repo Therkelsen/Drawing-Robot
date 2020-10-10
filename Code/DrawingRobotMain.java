f7753003c7433ae8f4c165ff243c4153b304b2af
Therkelsen <37107686+Therkelsen@users.noreply.github.com>
2020-10-10 17:33:17 +0200

    Nothing of importance

--- Code/DrawingRobotMain.java
+++ Code/DrawingRobotMain.java
@@ -10,17 +10,23 @@ public class DrawingRobotMain {
     public static void main(String[] args) {
         
         //  This is where we will put our main code
+        
+        System.out.println("====================");
+        System.out.println("Starting program");
+
         Picture danny = new Picture("Drawing-Robot/assets/danny.jpg");
         
         danny.show();
         
-        for (int col = 0; col < danny.width(); col++)
-            for (int row = 0; row < danny.height(); row++)
-                System.out.println("Coords [x,y]: [" + col + ","+ row 
+        for (int col = 1; col < danny.width(); col++)
+            for (int row = 1; row < danny.height(); row++)
+                System.out.println("Coords [x,y]: [" + col + ","+ row
                 + "] | Colors [r,g,b]: ["
-                + danny.get(col,row).getRed() + " "
-                + danny.get(col,row).getGreen() + " "
+                + danny.get(col,row).getRed() + ","
+                + danny.get(col,row).getGreen() + ","
                 + danny.get(col,row).getBlue() + "]");
+
+                System.out.println("Program done");
         
         
     }
