593063bb341787a92545961712046bf91b5766f7
Therkelsen <37107686+Therkelsen@users.noreply.github.com>
2020-10-29 00:00:01 +0100

    Code optimization
    Moved image processing into the DrawingRobot class, thus cleaning up the main method.

--- Code/DrawingRobotMain.java
+++ Code/DrawingRobotMain.java
@@ -1,12 +1,7 @@
 
 package dk.sdu.mmmi.rd1.edgedetect;
 
-import java.awt.Color;
-
 import DrawingRobot;
-import Picture.java;
-import Luminance.java;
-import EdgeDetector.java;
 
 public class DrawingRobotMain {
     public static void main(String[] args) {
@@ -14,37 +9,11 @@ public class DrawingRobotMain {
         System.out.println("====================");
         System.out.println("Starting program");
         
-        // * Colors needed
-        Color grayVal = new Color(0,0,0);
-        Color gray = new Color(100,100,100);
-        Color white = new Color(255,255,255);
-
-        // * Variables needed
-        int threshold = 60;
-        float grVal;
-        int draw;
-        int prevDraw = 1;
-
-        String instructions = "";
-
-        //int xVal = 0;
-        
-        // * Loading image and initializing an image to convert onto
-        Picture image = new Picture("assets/danny.jpg");
-        Picture convertedImage = new Picture(image.width(), image.height());
-        
-        //int[][] startDraw = new int[image.width()][2];
-        //int[][] endDraw = new int[image.width()][2];
-
-        //  Display the loaded image
-        image.show();
-        
         // * For every column in every row, where every other row iterates backwards
-        // * Convert image to grayscale, then convert to "black"/white
-        // * Then update frame displaying converted image, and add current instruction to string
 
-        for (int row = 0; row < image.height(); row++) {
+        for (int row = 0; row < DrawingRobot.image.height(); row++) {
             if (row % 2 != 0) {
+<<<<<<< Updated upstream
                 for (int col = 0; col < image.width(); col++) {
                     
                     grayVal = Luminance.toGray(image.get(col,row));
@@ -98,18 +67,18 @@ public class DrawingRobotMain {
                     }
                     
                     prevDraw = draw;
+=======
+                for (int col = 0; col < DrawingRobot.image.width(); col++) {
+                    DrawingRobot.processImage(col, row);
+                }
+            } else {
+                for (int col = DrawingRobot.image.width()-1; col >= 0; col--) {
+                    DrawingRobot.processImage(col, row);
+>>>>>>> Stashed changes
                 }
             }
         }
 
-        // * Print everything in the terminal
-        System.out.println("Instructions: ");
-        System.out.println(instructions);
-        System.out.println("End of instructions");
-        System.out.println("");
-
-        System.out.println("Length of string: " + instructions.length() + " characters");
-
-        System.out.println("Program done");
+        DrawingRobot.print();
     }
 }
