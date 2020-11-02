9cb5aff9b7faaf4cf4762f54c9ee13e7354e5a6f
Therkelsen <37107686+Therkelsen@users.noreply.github.com>
2020-11-02 11:06:12 +0100

    Update DrawingRobotMain.java

--- Code/DrawingRobotMain.java
+++ Code/DrawingRobotMain.java
@@ -13,68 +13,12 @@ public class DrawingRobotMain {
 
         for (int row = 0; row < DrawingRobot.image.height(); row++) {
             if (row % 2 != 0) {
-<<<<<<< Updated upstream
-                for (int col = 0; col < image.width(); col++) {
-                    
-                    grayVal = Luminance.toGray(image.get(col,row));
-                    convertedImage.set(col, row, grayVal);
-                    grVal = convertedImage.get(col,row).getRed();
-                    
-                    if (DrawingRobot.drawOrNot(grVal, threshold)) {
-                        convertedImage.set(col, row, gray);
-                        //startDraw[col][row] = 1;
-                    } else {
-                        convertedImage.set(col, row, white);
-                    }
-
-                    convertedImage.show();
-
-                    draw = DrawingRobot.drawOrNot(grVal, threshold) ? 1 : 0;
-                    
-                    if (draw == 1 && prevDraw == 0) {
-                        String s = String.valueOf(col) + ",";
-                        instructions = instructions.concat(s);
-                    } else if (draw == 0 && prevDraw == 1) {
-                        String s = String.valueOf(col) + ",";
-                        instructions = instructions.concat(s);
-                    }
-                    
-                    prevDraw = draw;
-                }
-            } else {
-                for (int col = image.width()-1; col >= 0; col--) {
-                    
-                    grayVal = Luminance.toGray(image.get(col,row));
-                    convertedImage.set(col, row, grayVal);
-                    grVal = convertedImage.get(col,row).getRed();
-
-                    if (DrawingRobot.drawOrNot(grVal, threshold)) {
-                        convertedImage.set(col, row, gray);
-                    } else {
-                        convertedImage.set(col, row, white);
-                    }
-                    
-                    convertedImage.show();
-
-                    draw = DrawingRobot.drawOrNot(grVal, threshold) ? 1 : 0;
-                    
-                    if (draw == 1 && prevDraw == 0) {
-                        String s = String.valueOf(col) + ",";
-                        instructions = instructions.concat(s);
-                    } else if (draw == 0 && prevDraw == 1) {
-                        String s = String.valueOf(col) + ",";
-                        instructions = instructions.concat(s);
-                    }
-                    
-                    prevDraw = draw;
-=======
                 for (int col = 0; col < DrawingRobot.image.width(); col++) {
                     DrawingRobot.processImage(col, row);
                 }
             } else {
                 for (int col = DrawingRobot.image.width()-1; col >= 0; col--) {
                     DrawingRobot.processImage(col, row);
->>>>>>> Stashed changes
                 }
             }
         }
