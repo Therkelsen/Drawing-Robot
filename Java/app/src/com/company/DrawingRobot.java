8cb956ad9c57bd7e9246f9053859f4560288f432
Therkelsen <37107686+Therkelsen@users.noreply.github.com>
2020-11-25 11:31:01 +0100

    Moved folder into IntelliJ project
    Co-Authored-By: AugustBuchert <72136981+AugustBuchert@users.noreply.github.com>
    Co-Authored-By: EmilThrane <72137546+EmilThrane@users.noreply.github.com>

---
+++ Java/app/src/com/company/DrawingRobot.java
@@ -0,0 +1,99 @@
+
+package com.company;
+
+import java.awt.Color;
+
+import java.util.ArrayList;
+
+public class DrawingRobot {
+
+    // * Colors needed
+    static Color grayVal = new Color(0,0,0);
+    static Color gray = new Color(100,100,100);
+    static Color white = new Color(255,255,255);
+
+    // * Variables needed
+    static int threshold = 60;
+    static float grVal;
+    static int draw = 0;
+    static int prevDraw = 0;
+
+    static String s = "";
+
+    // * Loading image and initializing an image to convert onto
+    static Picture image = new Picture("assets/test.png");
+
+    static ArrayList<String> inst = new ArrayList<String>();
+    static String instructions = "";
+
+    public static boolean drawOrNot(float gray, int threshold) {
+        
+        // *  If the intensity of gray is higher than the set limit
+        // *  Pencil down (Draw), if not, Pencil up (Stop drawing)
+        
+        if (gray < threshold) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void processImage(int col, int row) {
+        
+        // * Convert image to grayscale, then convert to "black"/white
+
+        grayVal = Luminance.toGray(image.get(col,row));
+        image.set(col, row, grayVal);
+        grVal = image.get(col,row).getRed();
+        
+        if (drawOrNot(grVal, threshold)) {
+            image.set(col, row, gray);
+        } else {
+            image.set(col, row, white);
+        }
+
+        // * Update frame displaying image, and add current instruction to string
+
+        image.show();
+
+        draw = drawOrNot(grVal, threshold) ? 1 : 0;
+
+        if (draw != prevDraw) {
+            if (draw == 1) {
+                s = String.valueOf(col + 1) + " " + String.valueOf(row + 1) + " " + String.valueOf(draw) + ",";
+            } else {
+                s = String.valueOf(col) + " " + String.valueOf(row + 1) + " " + String.valueOf(draw) + ",";
+            }
+
+            inst.add(s);
+        }
+
+        prevDraw = draw;
+    }
+
+    /*public static String instruct() {
+        for (int i = 0; i < inst.size(); i++) {
+            if (i % 2 == 0) {
+                instructions = instructions.concat(String.valueOf(inst.get(i)));
+                return instructions;
+            } else {
+                instructions = "";
+                instructions = instructions.concat(String.valueOf(inst.get(i)));
+            }
+        }
+        return "";
+    }*/
+
+    public static void print() {
+
+        // * Print everything in the terminal
+
+        System.out.println("End of instructions");
+        System.out.println("");
+
+        System.out.println("Length of instructions: [" + inst.size() + "] pairs");
+        System.out.println("Size of image: [" + image.width() + " * " + image.height() + "] px = [" + (image.width()*image.height()) + "] px");
+        
+    }
+
+}
---
+++ Java/app/src/com/company/DrawingRobotMain.java
@@ -0,0 +1,58 @@
+
+package com.company;
+
+public class DrawingRobotMain {
+    public static void main(String[] args) throws InterruptedException {
+        
+        System.out.println("====================");
+        System.out.println("Starting program");
+
+        RobotClient rc = new RobotClient("127.0.0.1", 80);
+        rc.connect();
+        System.out.println("Is connected: " + rc.isConnected());
+        
+        // * For every column in every row, where every other row iterates backwards
+
+        for (int row = 0; row < DrawingRobot.image.height(); row++) {
+            if (row % 2 == 0) {
+                for (int col = 0; col < DrawingRobot.image.width(); col++) {
+                    DrawingRobot.processImage(col, row);
+                    //System.out.println("Sending data");
+                    //rc.write("(" + (row+1) + ")" + "\n");
+                    //rc.write(DrawingRobot.instructions);
+                    //DrawingRobot.instructions = "";
+                    //System.out.println(col + ", " + row);
+                }
+            } else {
+                for (int col = DrawingRobot.image.width()-1; col >= 0; col--) {
+                    DrawingRobot.processImage(col, row);
+                    //System.out.println("Sending data");
+                    //rc.write("(" + (row+1) + ")" + "\n");
+                    //rc.write(DrawingRobot.instructions);
+                    //DrawingRobot.instructions = "";
+                    //System.out.println(col + ", " + row);
+                }
+            }
+            //Thread.sleep(500);
+        }
+
+        System.out.println("Instructions: ");
+
+        for (int i = 0; i < DrawingRobot.inst.size(); i++) {
+            if (i % 2 != 0) {
+                DrawingRobot.instructions = DrawingRobot.instructions.concat(String.valueOf(DrawingRobot.inst.get(i)));
+                rc.write(DrawingRobot.instructions + "\n");
+                System.out.println(DrawingRobot.instructions);
+                DrawingRobot.instructions = "";
+            } else {
+                DrawingRobot.instructions = DrawingRobot.instructions.concat(String.valueOf(DrawingRobot.inst.get(i)));
+            }
+            
+        }
+        
+        DrawingRobot.print();
+        
+        rc.disconnect();
+        System.out.println("Program done");
+    }
+}
